cda0839efbe10728eb2da2bf9cb7797be5724831
morycal <k95230387@gmail.com>
2026-06-22 03:33:59 +0330

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,12 +1,13 @@
 <?php
+include "db.php";
 
 $merchant_id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 
-$authority = $_GET["Authority"];
 $status = $_GET["Status"];
+$authority = $_GET["Authority"];
 
 if($status != "OK"){
-    die("پرداخت ناموفق بود ❌");
+  die("پرداخت ناموفق");
 }
 
 $amount = 10000 * 10;
@@ -18,25 +19,27 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-    "merchant_id" => $merchant_id,
-    "authority" => $authority,
-    "amount" => $amount,
+  "merchant_id"=>$merchant_id,
+  "authority"=>$authority,
+  "amount"=>$amount
 ]));
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json"
-]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
 
 $result = curl_exec($ch);
 curl_close($ch);
 
 $result = json_decode($result, true);
 
-if(isset($result["data"]["code"]) && $result["data"]["code"] == 100){
+if($result["data"]["code"] == 100){
 
-    echo "پرداخت موفق بود ✔<br>";
-    echo "کد پیگیری: " . $result["data"]["ref_id"];
+$ref = $result["data"]["ref_id"];
 
-} else {
-    echo "پرداخت تایید نشد ❌";
+$conn->query("INSERT INTO orders(amount,status,ref_id)
+VALUES($amount,'paid','$ref')");
+
+echo "پرداخت موفق ✔ <br> کد پیگیری: ".$ref;
+
+}else{
+echo "پرداخت ناموفق ❌";
 }
