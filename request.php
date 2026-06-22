18aa5ad01c7513a79a9851cd4523d28af63ec615
morycal <k95230387@gmail.com>
2026-06-22 03:33:38 +0330

    Update request.php

--- request.php
+++ request.php
@@ -1,11 +1,10 @@
 <?php
-
 $data = json_decode(file_get_contents("php://input"), true);
 
-$amount = $data["amount"] * 10; // تبدیل به ریال
-$callback = "http://localhost/shop/verify.php";
+$amount = $data["amount"] * 10;
+$merchant_id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 
-$merchant_id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"; // 🔴 مرچنت کد زرین‌پال
+$callback = "http://localhost/shop/verify.php";
 
 $ch = curl_init();
 
@@ -14,15 +13,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-    "merchant_id" => $merchant_id,
-    "amount" => $amount,
-    "callback_url" => $callback,
-    "description" => "خرید از فروشگاه",
+  "merchant_id"=>$merchant_id,
+  "amount"=>$amount,
+  "callback_url"=>$callback,
+  "description"=>"Order payment"
 ]));
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json"
-]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
 
 $result = curl_exec($ch);
 curl_close($ch);
@@ -30,8 +27,9 @@ curl_close($ch);
 $result = json_decode($result, true);
 
 if(isset($result["data"]["authority"])){
-    $url = "https://www.zarinpal.com/pg/StartPay/" . $result["data"]["authority"];
-    echo json_encode(["url"=>$url]);
-} else {
-    echo json_encode(["error"=>"failed"]);
+  echo json_encode([
+    "url"=>"https://www.zarinpal.com/pg/StartPay/".$result["data"]["authority"]
+  ]);
+}else{
+  echo json_encode(["error"=>"fail"]);
 }
